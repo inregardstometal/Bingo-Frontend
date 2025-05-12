308225d7f084e919784543f12432cf804b8fbcab
inregardstometal <inregardstometal@gmail.com>
2025-05-12 13:32:17 -0400

    more progress toward updating dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,11 @@
-export default {
+import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+export default defineConfig({
+    plugins:  [react(), tsconfigPaths()],
     server: {
         open: true
-    }
-}
+    },
+    
+});
