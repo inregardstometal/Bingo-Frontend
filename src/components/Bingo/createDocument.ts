b6107ec0f4e8e4c52a0210748916d3ae1a9fe574
inregardstometal <inregardstometal@gmail.com>
2025-05-15 14:31:36 -0400

    changed comment

--- src/components/Bingo/createDocument.ts
+++ src/components/Bingo/createDocument.ts
@@ -2,22 +2,28 @@ export const createDocument = <T extends HTMLElement>(
     els: T[],
     withStyles: boolean = true
 ): HTMLHtmlElement => {
+    //Create the main structural elements of an html document
     const html = document.createElement("html");
     const head = document.createElement("head");
     const body = document.createElement("body");
     let styles: Node[] = [];
+    //Rip the styled from the current document and clone them (to prevent mutation)
     if (withStyles) {
         styles = Array.from(document.getElementsByTagName("style")).map((el) =>
             el.cloneNode(true)
         );
     }
+    //Deep clone the document children to prevent mutation
     els = els.map((el) => el.cloneNode(true) as T);
 
+    //Write both styles and children into their corresponding parents
     head.append(...styles);
     body.append(...els);
 
+    //Prevent scrollbar from appearing
     body.style.overflow = "visible";
 
+    //Write head and body into HTML document
     html.appendChild(head);
     html.appendChild(body);
 
